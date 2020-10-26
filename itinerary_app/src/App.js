3dc50574266043a06a24fca62e62a14c0aec04cb
Aaron Li <aaronhf.li@mail.utoronto.ca>
2020-10-26 13:30:35 -0400

    Add react-router-dom and initialize routing in App.js

--- itinerary_app/src/App.js
+++ itinerary_app/src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
+import {Route, Switch, BrowserRouter} from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './react-components/Home';
+import Login from './react-components/Login';
+import Signup from './react-components/Signup';
+
+class App extends React.Component {
+  state = {
+    initialState: "Itinerary" //probably going to change this later
+  }
+
+  render(){
+    return(
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' render={() => 
+              (<Home appState={this.state}/>)}/>
+            <Route exact path='/login' render={() => 
+              (<Login appState={this.state}/>)}/>
+            <Route exact path='/signup' render={() => 
+              (<Signup appState={this.state}/>)}/>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
